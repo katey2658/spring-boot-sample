7bcf5a0bdb8f084e1d5a1bace86d8384bbc08503
王东 <wangdong@36kr.com>
2020-05-26 19:27:19 +0800

    demo

---
+++ spring-boot-sample-demo13/src/main/java/com/busyzero/demo/App.java
@@ -0,0 +1,19 @@
+package com.busyzero.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class App {
+
+    @RequestMapping("/")
+    private String index() {
+        return "My Dear";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
